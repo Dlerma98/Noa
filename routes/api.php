db82fe6a220039b6455ffebb5a4262ddb642143a
David Garcia Lerma <158084769+Dlerma98@users.noreply.github.com>
2025-02-21 18:07:58 +0100

    Creacion de api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AnalysisApiController;
+use App\Http\Controllers\Api\PostApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/posts', [PostApiController::class, 'index']);
+Route::get('/analyses', [AnalysisApiController::class, 'index']);

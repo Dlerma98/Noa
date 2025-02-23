e5d7fefc711c370fec4f99e4f7e01096ca90d909
David Garcia Lerma <158084769+Dlerma98@users.noreply.github.com>
2025-02-23 21:49:41 +0100

    Terminar traducciones español

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,7 +6,7 @@
     @endif
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Edit Post
+            {{ __('Edit a Post') }}
         </h2>
     </x-slot>
 
